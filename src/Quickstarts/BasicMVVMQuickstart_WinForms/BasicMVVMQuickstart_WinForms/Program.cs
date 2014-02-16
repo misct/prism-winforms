7e75e28eaa87d753c7c7f736b7500e110f16e9a6
Wayne Bloss <wayne.bloss@dsuccess.com>
2014-02-16 17:43:38 -0500

    Added BasicMVVMQuickstart_WinForms MainWindow view.

--- src/Quickstarts/BasicMVVMQuickstart_WinForms/BasicMVVMQuickstart_WinForms/Program.cs
+++ src/Quickstarts/BasicMVVMQuickstart_WinForms/BasicMVVMQuickstart_WinForms/Program.cs
@@ -15,7 +15,7 @@ namespace BasicMVVMQuickstart_WinForms
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			//Application.Run(new Form1());
+			Application.Run(new Views.MainWindow());
 		}
 	}
 }
